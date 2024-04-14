58b0bbcf01378033b5536c3b5ebce2c31935ece6
Matekar <kiewskimarcin@gmail.com>
2024-04-15 01:25:19 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import './global.css';
+</script>
+
+<div class="app">
+	<header>
+		<nav>
+			<h1>XaaS</h1>
+			<ul>
+				<!-- <li><a href="/tabela/cytowania">Cytowania</a></li>
+				<li><a href="/tabela/raa">Relacje Autor-Artykul</a></li>
+				<li><a href="/tabela/rra">Relacje Redaktor-Artykul</a></li> -->
+			</ul>
+		</nav>
+		<!-- <nav>
+			<h2>Tabele</h2>
+			
+		</nav> -->
+		<!-- <nav>
+			<h2>Widoki</h2>
+			<ul>
+				<li>
+					<a href="/widok/autorzy_artykuly">Autorzy i artykuły</a><a
+						href="/widok/redaktorzy_przypisania">Przypisania redaktorów</a
+					><a href="/widok/sfinalizowane_artykuly">Artykuły sfinalizowane</a>
+				</li>
+			</ul>
+		</nav> -->
+	</header>
+	<main>
+		<slot />
+	</main>
+	<footer>Marcin Kiżewski</footer>
+</div>
